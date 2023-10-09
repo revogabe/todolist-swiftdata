c71b912494cb81771226efdfe5cc84560f7e2a94
Daniel Gabriel <revogabe.dev@gmail.com>
2023-10-09 17:37:49 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Daniel Gabriel on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
